831fa4ab7918d76d4c09dceeff7705395069edaf
iva_kishko <ivakishko@gmail.com>
2023-05-15 22:25:48 +0400

    new

---
+++ 7KYU/coding-meetup.js
@@ -0,0 +1,6 @@
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+function countDevelopers(list) {
+    // your awesome code here :)
+    return list.filter(e => e.continent === 'Europe' && e.language === 'JavaScript').length
+  }
